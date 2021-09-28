2cbdcafc4561d7c4ad8708187b679005aafe388b
unknown <cobin065025@gmail.com>
2021-09-29 05:50:25 +0900

    first commit

---
+++ lolreplay/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from lolreplay.users.models import User
+from lolreplay.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
